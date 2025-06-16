030537ba0cf8931d9ba8a61afd676611aa3001b8
Pavel Shlyoma <pashcko.shlyoma@yandex.ru>
2025-06-16 17:42:19 +0300

    fix log

--- js/main.js
+++ js/main.js
@@ -1,33 +1,24 @@
-const getSidebar = document.querySelector('.sidebar')
+const getSidebar = document.querySelector('.sidebar');
+
+const getHamburger = document.querySelector('.hamburger');
 
 const getSidebarMenu = document.getElementById('sidebarMenu')
 
-function setSidebarOpen (sidebarOpen) {
-    sidebarOpen.style.width = '100%';
-    sidebarOpen.style.visibility = 'visible';
-}
+const getHeaderMenu = document.querySelector('.main')
 
-function setSidebarClose (sidebarOpen) {
-    sidebarOpen.style.width = '0';
-    sidebarOpen.style.visibility = 'hidden';
-}
+getHamburger.addEventListener('click', () => {
+    getHamburger.classList.toggle('active')
 
-function setSidebarMenuOpen (sidebarMenuOpen) {
-    sidebarMenuOpen.style.display = 'flex';
-}
+    if (getHamburger.classList.contains('active')) {
+        getSidebar.style.width = '350px';
+        getSidebar.style.visibility = 'visible';
+        getSidebarMenu.style.display = 'flex';
+        getHeaderMenu.style.marginLeft = "350px";
+    } else {
+        getSidebar.style.width = '0';
+        getSidebar.style.visibility = 'hidden';
+        getSidebarMenu.style.display = 'none';
+        getHeaderMenu.style.marginLeft = "0";
 
-function setSidebarMenuClose (sidebarMenuOpen) {
-    sidebarMenuOpen.style.display = 'none';
-}
-
-document.addEventListener("click", (event) => {
-    if (event.target.closest('.hamburger')) {
-        setSidebarOpen(getSidebar);
-        setSidebarMenuOpen(getSidebarMenu)
-    }
-
-    if (event.target.closest('.hamburger.active')) {
-        setSidebarClose(getSidebar);
-        setSidebarMenuClose(getSidebarMenu)
     }
 })
